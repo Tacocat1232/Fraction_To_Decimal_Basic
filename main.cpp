dae4ca07632ca5b9d7c4eadb23acee6d38017738
Tacocat1232 <62225436+Tacocat1232@users.noreply.github.com>
2022-03-22 08:01:26 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+double hat;
+double watch;
+double answer;
+
+std::cout << "Please input the numerator: \n";
+std::cin >> hat;
+
+std::cout << "Please input the denominator \n";
+std::cin >> watch;
+
+answer = hat / watch;
+
+std::cout << answer;
+
+
+}
